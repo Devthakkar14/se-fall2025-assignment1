6d5992a4bf478c0a2b796935d823f3292d665392
Dev Thakkar <devthakkar@10-20-111-149.dynapool.wireless.nyu.edu>
2025-10-15 14:38:39 -0400

    Add poll creation functionality
    - Added management command to create polls programmatically
    - Added sample poll creation to EB deployment
    - Poll: 'What is your favorite CI/CD tool?' with 5 choices
    - This will add a poll to the deployed site automatically

---
+++ polls/management/commands/add_poll.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from polls.models import Question, Choice
+
+
+class Command(BaseCommand):
+    help = "Add a sample poll to the database"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--question',
+            type=str,
+            default="What's your favorite programming language?",
+            help='The question text'
+        )
+
+    def handle(self, *args, **options):
+        question_text = options['question']
+        
+        # Create the question
+        question = Question.objects.create(
+            question_text=question_text,
+            pub_date=timezone.now(),
+        )
+        
+        # Create some default choices
+        choices = [
+            "Python",
+            "JavaScript", 
+            "Java",
+            "C++",
+            "Other"
+        ]
+        
+        for choice_text in choices:
+            Choice.objects.create(
+                question=question,
+                choice_text=choice_text,
+                votes=0
+            )
+        
+        self.stdout.write(
+            self.style.SUCCESS(f'Successfully created poll: "{question_text}"')
+        )
